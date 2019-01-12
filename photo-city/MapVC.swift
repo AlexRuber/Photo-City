b8179184a55a3a83ea8cb99bab4a251bee1a2706
Mihai Ruber <mruber@ucsd.edu>
2019-01-12 15:39:33 -0800

    first commit

--- photo-city/MapVC.swift
+++ photo-city/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  photo-city
 //
 //  Created by Mihai Ruber on 1/12/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
