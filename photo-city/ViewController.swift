a58ce4dff60a20576c5ad3851431f8fcdae6f2d3
Mihai Ruber <mruber@ucsd.edu>
2019-01-12 15:29:27 -0800

    Initial Commit

---
+++ photo-city/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photo-city
+//
+//  Created by Mihai Ruber on 1/12/19.
+//  Copyright © 2019 Mihai Ruber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
