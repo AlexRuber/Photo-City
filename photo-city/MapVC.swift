934e3ee3c4bc4320587364b4d75fc8ef51240524
Mihai Ruber <mruber@ucsd.edu>
2019-01-12 17:26:12 -0800

    added the mapview, buttons, and tap gesture functionality

--- photo-city/MapVC.swift
+++ photo-city/MapVC.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
+import MapKit
+import CoreLocation
 
-class MapVC: UIViewController {
+class MapVC: UIViewController,UIGestureRecognizerDelegate {
+    
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // Variables
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
+        addDoubleTap()
+    }
+    
+    // Functions
+    
+    private func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        mapView.addGestureRecognizer(doubleTap)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func centerMap(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
+}
 
+extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    @objc func dropPin() {
+        // drop the pin on the map
+        print("pin was dropped")
+    }
+}
 
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
 
